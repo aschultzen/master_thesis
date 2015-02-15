13ec187b36aed512add9c461e105bbcb2e07e219
Aril Schultzen <aschultzen@gmail.com>
2015-02-15 22:46:15 +0100

    Fixed some scrolling issues

--- arduino/lcd_display/lcd_display.ino
+++ arduino/lcd_display/lcd_display.ino
@@ -11,54 +11,118 @@
 #define D5_pin  5
 #define D6_pin  6
 #define D7_pin  7
-int i = 0;
 
 LiquidCrystal_I2C	lcd(I2C_ADDR,En_pin,Rw_pin,Rs_pin,D4_pin,D5_pin,D6_pin,D7_pin);
 
 void setup()
 {
   lcd.begin (16,2);
-// Switch on the backlight
-lcd.setBacklightPin(BACKLIGHT_PIN,POSITIVE);
-lcd.setBacklight(HIGH);
-lcd.home (); // go home
-lcd.print("Counting: ");
+  lcd.setBacklightPin(BACKLIGHT_PIN,POSITIVE);
+  lcd.setBacklight(HIGH);
+  lcd.home (); // go home
+  lcd.print("   * READY *   ");
+  Serial.begin(9600);
+  Serial.println("IM_UP");
+}
+String content = "";
+char character;
+int curs = 0;
+int myDelay = 1000;
+void loop()
+{
+  while(Serial.available()) {
+    character = Serial.read();
+    content.concat(character);
+
+    if (content.endsWith("END")) {
+      Serial.println(content);
+      display_text(content.substring(0,(content.length()-3)),curs);
+      content="";
+    }
+    if (content.endsWith("CLEAR")) {
+      Serial.println("Buffer cleared..");
+      content="";
+    } 
+
+    if (content.equals("SWITCH LINE")) {
+      curs++;
+      curs = curs % 2; 
+      Serial.println(curs);
+      content="";
+    }
+   
+   if (content.equals("SPEED UP")) {
+      myDelay = myDelay - 100;
+      Serial.println(myDelay);
+      content="";
+    } 
+
+  }
+}
+
+String getWordFromSerial()
+{
+  byte mybyte = 0;
+  char buffer[128];
+  int i = 0;
+  if(Serial.available()>0){
+    buffer[i] = (char)Serial.read();
+    i++;
+
+    if(&buffer[i-1]=="\\"){
+      char temp = Serial.read();
+      i++;
+      buffer[i] = temp;
+
+      if(temp == 'n'){
+        String str(buffer);
+        //return str;
+        Serial.println(str);
+      }
+    }
+  }
 }
 
 void display_text(String incoming, int row)
 {
-  int x = 0;
-  int y = 0;
-  
-  if(row == 2){
-   y = 1;
-  } 
-  lcd.setCursor (x,y);          //Adjust the cursor for the desired pos.
-  lcd.print("                ");    //Clearing
-  lcd.setCursor (x,y);          //Re-Adjusting
-  lcd.print(incoming);          //Printing
+  if((incoming.length()<16))
+  {
+    int x = 0;
+    lcd.setCursor (x,row);              //Adjust the cursor for the desired pos.
+    lcd.print("                ");    //Clearing
+    lcd.setCursor (x,row);              //Re-Adjusting
+    lcd.print(incoming);              //Printing
+  }
+  else
+  {
+     int i;
+     int x = incoming.length() - 16;
+     for (i = 0; i < x+2; i++){
+     String sub = incoming.substring(i,i+16);
+     delay(myDelay);
+     lcd.setCursor(0,row);            
+     lcd.print(sub);
+     Serial.println(sub);
+     }
+   }
 }
 
-void loop()
-{
-  lcd.setCursor (0,1);        // go to start of 2nd line
-  lcd.print(i++,DEC);
-}
 
 void smiley()
 {
   byte smiley[8] = {
-  B00000,
-  B00000,
-  B00000,
-  B00000,
-  B00000,
-  B00000,
-  B00000,
+    B00000,
+    B00000,
+    B00000,
+    B00000,
+    B00000,
+    B00000,
+    B00000,
   };
-  
+
   lcd.createChar(0,smiley);
   lcd.begin(16, 2);  
   lcd.write(byte(0));
 }
+
 
