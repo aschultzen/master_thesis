c3045c6d274331009b1070b83ab86dcb7e8b06c4
Aril Schultzen <aschultzen@gmail.com>
2015-08-10 11:35:26 +0200

    Added functions for database connection

--- tidsdata_db.py
+++ tidsdata_db.py
@@ -1,7 +1,13 @@
+# NOTES:
+# Used Python v.3.4.3
+# and mysql-connector-python-2.0.4-py3.4
+
 import win32service
 import win32serviceutil
-import win32event
-import ctypes  # An included library with Python install.
+import win32event 
+import ctypes
+import mysql.connector
+
 
 class PySvc(win32serviceutil.ServiceFramework):
     # you can NET START/STOP the service by the following name
@@ -34,6 +40,38 @@ class PySvc(win32serviceutil.ServiceFramework):
         self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
         # fire the stop event
         win32event.SetEvent(self.hWaitStop)
+
+# BUG: Does not care if file is
+# bigger than memory. Could cause
+# "memoutofbound"
+def path2list(path):
+    result = open(path, 'r')
+    return result.read()
+    close.result
+
+def dbConnect():
+    try:    
+        dbConnection = mysql.connector.connect(host='10.1.0.232',database='clock_data',user='monitor',password='monitor')
         
+        if dbConnection.is_connected():
+            print("Connection to database (" + dbConnection.server_host +":" + str(dbConnection.server_port) + ") established")
+
+    except Error as e:
+            print(e)
+
+    return dbConnection    
+
+def dbClose(dbConnection):
+    dbConnection.close()
+
+    if dbConnection.is_connected():
+            print("Connection to database (" + dbConnection.server_host +":" + str(dbConnection.server_port) + ") NOT closed") 
+    else:
+        print("Connection to database closed")
+
 if __name__ == '__main__':
-    win32serviceutil.HandleCommandLine(PySvc)
+    #win32serviceutil.HandleCommandLine(PySvc)
+    #somelist = path2list("C:\\test2.dat")
+    #print (somelist)
+    db_con = dbConnect()
+    dbClose(db_con)
