4725e8618056615c3510f2b38513ef205ce086ba
Aril Schultzen <aschultzen@gmail.com>
2015-09-14 22:42:44 +0200

    Made sure python can play nice with the same format as TimeLab

---
+++ unik4430/notes/assignment2.py
@@ -0,0 +1,37 @@
+# Typical usage
+# First argument: Reference data
+# Second argument: Data to filter based on reference
+# The program will also make a file in the programs root
+# with the result file 
+#python .\assignment2.py ..\Exercises\GPSDO\data\gps.dat ..\Exercises\GPSDO\data\ocxo.dat
+
+import ctypes
+import configparser
+import fileinput, sys
+import datetime
+import time
+import os
+import sys
+
+def getFile(path):
+	result = open(path, 'r')
+	resultList = result.read()
+	resultList = resultList.splitlines()
+	result.close()
+	resultList = [float(i) for i in resultList]
+	return resultList
+
+def filter(file1):
+	length = len(file1)
+	f_out = open('output', 'w+')
+	count = 0
+	while(count < length):
+		f_out.write(str(file1[count]) + "\n")
+		count = count + 1
+	f_out.close()
+
+
+if __name__ == '__main__':
+	file_1 = getFile(sys.argv[1])
+	#file_2 = getFile(sys.argv[2])
+	filter(file_1)
