4853005952e223816b0ad6b7e5ab7c7d2936e30b
Aril Schultzen <aschultzen@gmail.com>
2015-09-22 15:09:48 +0200

    started work on averaging

--- unik4430/notes/assignment2.py
+++ unik4430/notes/assignment2.py
@@ -1,10 +1,3 @@
-# Typical usage
-# First argument: Reference data
-# Second argument: Data to filter based on reference
-# The program will also make a file in the programs root
-# with the result file 
-#python .\assignment2.py ..\Exercises\GPSDO\data\gps.dat ..\Exercises\GPSDO\data\ocxo.dat
-
 import ctypes
 import configparser
 import fileinput, sys
@@ -13,16 +6,63 @@ import time
 import os
 import sys
 
+def t_print(message):
+    current_time = datetime.datetime.now().time()
+    complete_message = "[" + current_time.isoformat() + "] " +"[" + message + "]"
+    print(complete_message)
+
+def getConfVal(column, value):
+	config = configparser.ConfigParser()
+	configFile = "assignment2.ini"
+	config.read(configFile)
+	conf_value = config[column][value]
+	return conf_value
+
 class average:
-	count = 0 # Number of times called; n.
-	avg = 0
+	count = 0.0 # Number of times called; n.
+	total = 0.0	# n-1 + n + n +1
+	avg = 0.0	# The current average
+		
+	def pushpeek(self, value_n):
+		self.push(value_n) 
+		return self.avg
 
-	def __init__(self):
-		average.count = average.count + 1
+	def get_count(self):
+		return self.count
+	
+	def peek(self):
+		return self.avg
 
-	def get_average(value_n):
-		average.avg = (average.avg + value_n) / average.count 
-		return average.count
+	def push(self, value_n):
+		self.count = self.count + 1
+		self.total = self.total + value_n
+		self.avg = self.total / self.count
+
+	def get_total(self):
+		return self.total
+
+class smart_average:
+	count = 0.0 # Number of times called; n.
+	avg = 0.0	# The current average
+	prev_avg = 0.0 # The previous average
+		
+	def pushpeek(self, value_n):
+		self.push(value_n) 
+		return self.avg
+
+	def get_count(self):
+		return self.count
+	
+	def peek(self):
+		return self.avg
+
+	def push(self, value_n):
+		if(self.count == 0.0):
+			self.avg = value_n
+		else:
+			self.prev_avg = self.avg
+			self.avg = ( ( (self.count-1) * self.prev_avg) + value_n ) / self.count
+		self.count = self.count + 1
 
 def getFile(path):
 	result = open(path, 'r')
@@ -32,16 +72,28 @@ def getFile(path):
 	resultList = [float(i) for i in resultList]
 	return resultList
 
-def filter(file1):
-	length = len(file1)
-	f_out = open('output', 'w+')
+
+## Debug method
+def write_average_file(input_file, output_name, type):
+	t_print("Making average file...")
+	if(type == "smart"):
+		t_print("Using smart_average")
+		avg = smart_average()
+	else:
+		t_print("Using average")
+		avg = average()
+	
+	length = len(input_file)
+	file_out = open(output_name, 'w+')
 	count = 0
 	while(count < length):
-		f_out.write(str(file1[count]) + "\n")
+		file_out.write(str(avg.pushpeek(input_file[count])) + "\n")
 		count = count + 1
-	f_out.close()
+	file_out.close()
+	t_print("Finished")
 
 if __name__ == '__main__':
-	file_1 = getFile(sys.argv[1])
-	#file_2 = getFile(sys.argv[2])
-	filter(file_1)
+	file_1 = getFile(getConfVal("files","file2"))
+	write_average_file(file_1, "ocxo_avg_smart", "smart")
+	write_average_file(file_1, "ocxo_avg_dumb", "dumb")
+
