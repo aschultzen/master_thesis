4ca9b76c8cf260121f3243e33252950618b45b6b
Aril Schultzen <aschultzen@gmail.com>
2015-09-22 16:55:20 +0200

    Update

--- unik4430/notes/assignment2.py
+++ unik4430/notes/assignment2.py
@@ -22,29 +22,12 @@ class average:
 	count = 0.0 # Number of times called; n.
 	total = 0.0	# n-1 + n + n +1
 	avg = 0.0	# The current average
-		
-	def pushpeek(self, value_n):
-		self.push(value_n) 
-		return self.avg
-
-	def get_count(self):
-		return self.count
-	
-	def peek(self):
-		return self.avg
-
-	def push(self, value_n):
-		self.count = self.count + 1
-		self.total = self.total + value_n
-		self.avg = self.total / self.count
-
-	def get_total(self):
-		return self.total
-
-class smart_average:
-	count = 0.0 # Number of times called; n.
-	avg = 0.0	# The current average
 	prev_avg = 0.0 # The previous average
+	smart_switch = 0
+
+	def __init__(self, smart_switch):
+		self.smart_switch = smart_switch
+		t_print("Smart switch: " + str(smart_switch))
 		
 	def pushpeek(self, value_n):
 		self.push(value_n) 
@@ -58,10 +41,14 @@ class smart_average:
 
 	def push(self, value_n):
 		if(self.count == 0.0):
-			self.avg = value_n
+				self.avg = value_n
 		else:
-			self.prev_avg = self.avg
-			self.avg = ( ( (self.count-1) * self.prev_avg) + value_n ) / self.count
+			if(self.smart_switch == 0): # Naive but accurate method
+				self.total = self.total + value_n
+				self.avg = self.total / self.count
+			else: 						# The best method
+				self.prev_avg = self.avg
+				self.avg = ( ( (self.count-1) * self.prev_avg) + value_n ) / self.count 
 		self.count = self.count + 1
 
 def getFile(path):
@@ -74,15 +61,8 @@ def getFile(path):
 
 
 ## Debug method
-def write_average_file(input_file, output_name, type):
-	t_print("Making average file...")
-	if(type == "smart"):
-		t_print("Using smart_average")
-		avg = smart_average()
-	else:
-		t_print("Using average")
-		avg = average()
-	
+def write_average_file(input_file, output_name, a_type):
+	avg = average(a_type)	
 	length = len(input_file)
 	file_out = open(output_name, 'w+')
 	count = 0
@@ -94,6 +74,6 @@ def write_average_file(input_file, output_name, type):
 
 if __name__ == '__main__':
 	file_1 = getFile(getConfVal("files","file2"))
-	write_average_file(file_1, "ocxo_avg_smart", "smart")
-	write_average_file(file_1, "ocxo_avg_dumb", "dumb")
+	write_average_file(file_1, "ocxo_avg_smart", 1)
+	write_average_file(file_1, "ocxo_avg_dumb", 0)
 
