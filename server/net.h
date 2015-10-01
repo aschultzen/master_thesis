ecaeda5045d5bc62facdf78044c039c82e054aef
Aril Schultzen <aschultzen@gmail.com>
2015-10-01 12:15:15 +0200

    Moved files into server

---
+++ server/net.h
@@ -0,0 +1,49 @@
+#ifndef NET_H
+#define NET_H
+
+//Strict C
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <unistd.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <errno.h>
+#include <stdarg.h>
+#include <signal.h>
+#include <sys/wait.h>
+//#include <sys/time.h>
+
+// Mine
+#include "utils.h"
+
+/*
+* Starts the server.
+* Takes a port as param.
+*/
+int start_server(int port);
+
+//Size 32 bytes
+struct session_info{
+	int session_fd;		/* 4 B */
+	int client_id;		/* 4 B */
+	void *iobuffer; 	/* 8 B */
+	struct timeval tv; 	/* 16 B */
+} __attribute__ ((packed));
+
+/* GENERAL */
+#define BUFFER_SIZE 512
+#define TIME_OUT 5
+#define CLIENT_MAX 10
+#define MONITOR_MAX 2
+
+/* PROTOCOL */
+#define DISCONNECT "DISCONNECT"
+#define GET_TIME "GET_TIME"
+#define IDENTIFY "IDENTIFY"
+#define STORE "STORE"
+
+#endif /* !NET_H */
---
+++ server/utils.h
@@ -0,0 +1,13 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+/*
+* DIE function
+*/
+void die (int line_number, const char * format, ...);
+
+#endif /* !UTILS_H */
