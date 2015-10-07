5ca6fcccbb6a7e30aab96be6d08af272cd323379
Aril Schultzen <aschultzen@gmail.com>
2015-10-07 16:01:07 +0200

    Added timestamp print

--- server/utils.c
+++ server/utils.c
@@ -9,3 +9,41 @@ void die (int line_number, const char * format, ...)
     fprintf (stderr, ".\n");
     exit(1);
 }
+
+char *get_ip_str(const struct sockaddr *sa, char *s, size_t maxlen)
+{
+    switch(sa->sa_family) {
+        case AF_INET:
+            inet_ntop(AF_INET, &(((struct sockaddr_in *)sa)->sin_addr),
+                    s, maxlen);
+            break;
+
+        case AF_INET6:
+            inet_ntop(AF_INET6, &(((struct sockaddr_in6 *)sa)->sin6_addr),
+                    s, maxlen);
+            break;
+
+        default:
+            strncpy(s, "Unknown AF", maxlen);
+            return NULL;
+    }
+
+    return s;
+}
+
+void print_timestamp(const char* format, ...){
+	char buffer[100];
+	time_t rawtime;
+	struct tm *info;
+	time(&rawtime);
+	info = gmtime(&rawtime);
+
+	//sprintf(buffer, "[%02d:%02d:%02d] ", (info->tmyear, info->tm_hour)%24, info->tm_min, info->tm_sec);
+	strftime(buffer,80,"[%x - %X] ", info);
+
+	va_list argptr;
+    va_start(argptr, format);
+    fputs(buffer, stdout);
+    vfprintf(stdout, format, argptr);
+    va_end(argptr);
+}
--- server/utils.h
+++ server/utils.h
@@ -4,10 +4,12 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <time.h>
 
-/*
-* DIE function
-*/
 void die (int line_number, const char * format, ...);
+char *get_ip_str(const struct sockaddr *sa, char *s, size_t maxlen);
+void print_timestamp(const char* format, ...);
 
 #endif /* !UTILS_H */
