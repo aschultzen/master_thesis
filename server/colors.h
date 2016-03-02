c310a0c6270888962f8e8943a07d626a100ee1f7
Aril Schultzen <aschultzen@gmail.com>
2016-03-02 14:52:27 +0100

    Added header file with colors for coloring terminal output

---
+++ server/colors.h
@@ -0,0 +1,17 @@
+#ifndef COLORS_H
+#define COLORS_H
+
+/* RESET */
+#define RESET "\033[0m"
+
+/* COLORS */
+#define BLK_WHT "\033[030;47m"
+
+/* BOLD */
+#define BOLD_BLK_WHT "\033[1;030;47m"
+#define BOLD_GRN_BLK "\033[1;032;40m"
+
+/* UNDERLINED */
+#define UNDER_RED_BLACK "\033[4;031;40m"
+
+#endif /* !COLORS_H */
